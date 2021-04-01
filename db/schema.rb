c1676487b54462008894abcdf6ccdf326a50d0ed
uimarshall <uimarshall@gmail.com>
2021-04-01 13:25:23 +0100

    Add measurement model and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_122138) do
+ActiveRecord::Schema.define(version: 2021_04_01_122336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 2021_04_01_122138) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "measurements", force: :cascade do |t|
+    t.date "date_m"
+    t.integer "units"
+    t.bigint "user_id", null: false
+    t.bigint "course_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_measurements_on_course_id"
+    t.index ["user_id"], name: "index_measurements_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -31,4 +42,6 @@ ActiveRecord::Schema.define(version: 2021_04_01_122138) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "measurements", "courses"
+  add_foreign_key "measurements", "users"
 end
