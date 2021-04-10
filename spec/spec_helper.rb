998f04612acab65a4570db8e309ca3624badc4e6
uimarshall <uimarshall@gmail.com>
2021-04-10 14:59:02 +0100

    add configurations for rspec
    The configurations include database_cleaner, factory_bot shoulda-matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,10 +13,39 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+# require database_cleaner at the top level
+require 'database_cleaner'
+# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+# ...
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+
+  # ...
+  # add `FactoryBot` methods
+  config.include FactoryBot::Syntax::Methods
+  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+  # start the transaction strategy as examples are run
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+  # ...
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
