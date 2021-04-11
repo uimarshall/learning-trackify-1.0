1fa86810ece99ba75e41ece5bab050b27a5b1c44
uimarshall <uimarshall@gmail.com>
2021-04-11 16:20:03 +0100

    Add more tests to measurement model

--- spec/models/measurement_spec.rb
+++ spec/models/measurement_spec.rb
@@ -1,9 +1,11 @@
 RSpec.describe Measurement, type: :model do
-  describe "associations" do
-     it { should belong_to(:course) }
-  it { should belong_to(:user) }
+  describe 'associations' do
+    it { should belong_to(:course) }
+    it { should belong_to(:user) }
+    it { should have_db_column(:user_id) }
+    it { should have_db_column(:course_id) }
   end
-  describe "validations" do
+  describe 'validations' do
     it { is_expected.to validate_presence_of(:units) }
     it { is_expected.to validate_presence_of(:date_m) }
   end
