cfda327f9e187301acf66f62cf01eec52639c062
uimarshall <uimarshall@gmail.com>
2021-04-20 15:50:35 +0100

    Add __tests__ for Course component

--- client/src/__tests__/components/Measurement.js
+++ client/src/__tests__/components/Measurement.js
@@ -1,7 +1,8 @@
 import '@testing-library/jest-dom';
 
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
+import renderer from 'react-test-renderer'
 import Measurement from '../../components/Measurement';
 
 jest.mock('../../components/Measurement', () => () => <div id="Measurement">Measurement</div>);
@@ -14,5 +15,14 @@ describe('Measurement Component', () => {
     expect(mockComponent).not.toHaveTextContent(/^dragon$/); // to match the whole content
     expect(mockComponent).not.toHaveTextContent(/fire$/i); // to use case-insensitive match
     expect(mockComponent).toHaveTextContent('Measurement');
+  });
+});
+
+describe('Measurement Component', () => {
+  afterEach(cleanup);
+
+  it('should take a snapshot for Measurement Component', () => {
+    const asFragment = renderer.create(<Measurement />).toJSON()
+    expect(asFragment).toMatchSnapshot();
   });
 });
